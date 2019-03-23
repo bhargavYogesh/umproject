395473114e5ffb9840c22db1ef70186a4674400d
bhargavYogesh <47472492+bhargavYogesh@users.noreply.github.com>
2019-03-23 07:23:17 +0530

    sensor data
    .

--- DHT11_final_.ino
+++ DHT11_final_.ino
@@ -2,7 +2,7 @@
 #include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
 #define DHTTYPE DHT11   // DHT 11
 
-#define dht_dpin 0
+#define dht_dpin 0       //input pin d3=data
 DHT dht(dht_dpin, DHTTYPE); 
 void setup(void)
 { 
