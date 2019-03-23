98c1194973abfcd9bd8057a50b533e7ada9eff6b
bhargavYogesh <47472492+bhargavYogesh@users.noreply.github.com>
2019-03-23 07:19:06 +0530

    Add files via upload
    read sensor data

---
+++ DHT11_final_.ino
@@ -0,0 +1,29 @@
+
+#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
+#define DHTTYPE DHT11   // DHT 11
+
+#define dht_dpin 0
+DHT dht(dht_dpin, DHTTYPE); 
+void setup(void)
+{ 
+  dht.begin();
+  Serial.begin(9600);
+  Serial.println("Humidity and temperature\n\n");
+  delay(700);
+
+}
+void loop() {
+    float h = dht.readHumidity();
+    float t = dht.readTemperature(); 
+    float F = dht.readTemperature();         
+    Serial.print("Current humidity = ");
+    Serial.print(h);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(t); 
+    Serial.print("C  ");
+    Serial.print("temperature in Fahrenheit is = ");
+    Serial.print((F*9/5)+32);
+    Serial.println("F  ");  
+  delay(8000);
+}
