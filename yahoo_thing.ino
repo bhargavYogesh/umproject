1cb4bd0a0298aad3f4e8190fda17bca7d7a3eb44
bhargavYogesh <47472492+bhargavYogesh@users.noreply.github.com>
2019-04-20 13:12:18 +0530

    new tweaks
    reads sensor data to things speak.com and at the same time retrieve  JSON file from open-weather.org  
    it also hosts a COOL  local webpage with awesome fonts

---
+++ yahoo_thing.ino
@@ -0,0 +1,442 @@
+ /*
+  * Author: Emmanuel Odunlade 
+  * Complete Project Details https://randomnerdtutorials.com          1277333
+  */
+  
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include "DHT.h"
+
+#define DHTTYPE DHT11   // DHT 11
+
+String apiKey1 = "ZRGTMEOM2WZSLARM";
+const char* serverq = "api.thingspeak.com";
+// Replace with your SSID and password details
+char ssid[] = "bhargav";        
+char pass[] = "bhargaqwe";   
+
+ESP8266WebServer serverr(80);
+
+uint8_t DHTPin = D2; 
+
+DHT dht(DHTPin, DHTTYPE); 
+
+float Temperature;
+float Humidity;
+
+WiFiClient client;
+
+// Open Weather Map API server name
+const char server[] = "api.openweathermap.org";
+
+// city and country code
+String nameOfCity = "Bangalore, IN"; 
+
+// Replace the next line with your API Key
+String apiKey = "b5c34829f4340d8a3c3cb867f303f2bd"; 
+
+String text;
+
+int jsonend = 0;
+boolean startJson = false;
+int status = WL_IDLE_STATUS;
+
+
+
+#define JSON_BUFF_DIMENSION 2500
+
+unsigned long lastConnectionTime = 10 * 60 * 1000;     // last time you connected to the server, in milliseconds
+const unsigned long postInterval = 10 * 60 * 1000;  // posting interval of 10 minutes  (10L * 1000L; 10 seconds delay for testing)
+
+void setup() {
+  
+  Serial.begin(9600);
+  delay(100);
+  
+  pinMode(DHTPin, INPUT);
+
+  dht.begin(); 
+  
+  text.reserve(JSON_BUFF_DIMENSION);
+  
+  WiFi.begin(ssid,pass);
+  Serial.println("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi Connected");
+  printWiFiStatus();
+   serverr.on("/", handle_OnConnect);
+  serverr.onNotFound(handle_NotFound);
+
+  serverr.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() { 
+  serverr.handleClient();
+  //OWM requires 10mins between request intervals
+  //check if 10mins has passed then conect again and pull
+  if (millis() - lastConnectionTime > postInterval) {
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+    makehttpRequest();
+
+     float h = dht.readHumidity();           //thingspeak read start here
+      float t = dht.readTemperature();
+      
+              if (isnan(h) || isnan(t)) 
+                 {
+                     Serial.println("Failed to read from DHT sensor!");
+                      return;
+                 }
+
+                         if (client.connect(serverq,80))   //   "184.106.153.149" or api.thingspeak.com
+                      {  
+                            
+                             String postStr = apiKey1;
+                             postStr +="&field1=";
+                             postStr += String(t);
+                             postStr +="&field2=";
+                             postStr += String(h);
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey1+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                             Serial.print("Temperature: ");
+                            Serial.print(t);
+                             Serial.print(" degrees Celcius, Humidity: ");
+                             Serial.print(h);
+                             Serial.println("%. Send to Thingspeak.");
+                        }
+          client.stop();
+ 
+          Serial.println("Waiting...");
+  
+  // thingspeak needs minimum 15 sec delay between updates, i've set it to 30 seconds
+  delay(100);
+
+    
+    
+  }
+ 
+}
+
+void handle_OnConnect() {
+
+ Temperature = dht.readTemperature(); // Gets the values of the temperature
+  Humidity = dht.readHumidity(); // Gets the values of the humidity 
+  serverr.send(200, "text/html", SendHTML(Temperature,Humidity)); 
+}
+
+void handle_NotFound(){
+  serverr.send(404, "text/plain", "Not found");
+}
+
+// print Wifi status
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+// to request data from OWM
+void makehttpRequest() {
+  // close any connection before send a new request to allow client make connection to server
+  client.stop();
+
+  // if there's a successful connection:
+  if (client.connect(server, 80)) {
+    Serial.println("today weather be like...");
+    client.println("GET /data/2.5/forecast/hourly?q=" + nameOfCity + "&APPID=" + apiKey + "&mode=json&units=metric&cnt=2 HTTP/1.1");
+    client.println("Host: api.openweathermap.org");
+    client.println("User-Agent: ArduinoWiFi/1.1");
+    client.println("Connection: close");
+    client.println();
+    
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println(">>> Client Timeout !");
+        client.stop();
+        return;
+      }
+    }
+    
+    char c = 0;
+    while (client.available()) {
+      c = client.read();
+      // since json contains equal number of open and close curly brackets, this means we can determine when a json is completely received  by counting
+      // the open and close occurences,
+      //Serial.print(c);
+      if (c == '{') {
+        startJson = true;         // set startJson true to indicate json message has started
+        jsonend++;
+      }
+      if (c =='}') {
+        jsonend--;
+      }
+      if (startJson == true) {
+        text += c;
+      }
+      
+      // if jsonend = 0 then we have have received equal number of curly braces 
+      if (jsonend == 0 && startJson == true) {
+        parseJson(text.c_str());  // parse c string text in parseJson function
+        text = "";                // clear text string for the next time
+        startJson = false;        // set startJson to false to indicate that a new message has not yet started
+      }
+    }
+    //Serial.print(text);                       //my test
+  }
+  else {
+    // if no connction was made:
+    Serial.println("connection failed");
+    return;
+  }
+}
+
+//to parse json data recieved from OWM
+void parseJson(const char * jsonString) {
+  //StaticJsonBuffer<4000> jsonBuffer;
+  //const size_t bufferSize = 2*JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(2) + 4*JSON_OBJECT_SIZE(1) + 3*JSON_OBJECT_SIZE(2) + 3*JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 2*JSON_OBJECT_SIZE(7) + 2*JSON_OBJECT_SIZE(8) + 720;
+  DynamicJsonBuffer jsonBuffer(1000);
+ Serial.print(jsonString);
+  // FIND FIELDS IN JSON TREE
+  JsonObject& root = jsonBuffer.parseObject(jsonString);
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+  
+  JsonArray& list = root["list"];
+  JsonObject& nowT = list[0];
+  JsonObject& later = list[1];
+
+  // including temperature and humidity for those who may wish to hack it in
+  
+  String city = root["city"]["name"];
+  
+  float tempNow = nowT["main"]["temp"];
+  float humidityNow = nowT["main"]["humidity"];
+  String weatherNow = nowT["weather"][0]["description"];
+
+  float tempLater = later["main"]["temp"];
+  float humidityLater = later["main"]["humidity"];
+  String weatherLater = later["weather"][0]["description"];
+
+  // checking for four main weather possibilities
+  diffDataAction(weatherNow, weatherLater, "clear");
+  diffDataAction(weatherNow, weatherLater, "rain");
+  diffDataAction(weatherNow, weatherLater, "clouds");
+  diffDataAction(weatherNow, weatherLater, "fog");
+  
+  Serial.println();
+}
+
+//representing the data
+void diffDataAction(String nowT, String later, String weatherType) {
+  int indexNow = nowT.indexOf(weatherType);
+  int indexLater = later.indexOf(weatherType);
+  // if weather type = rain, if the current weather does not contain the weather type and the later message does, send notification
+  if (weatherType == "rain") { 
+    if (indexNow == -1 && indexLater != -1) {
+      
+      Serial.println("Oh no! It is going to " + weatherType + " later! Predicted " + later);         //!!!!!!!!!!!!!!!!!!!!
+    }
+  }
+  // for snow
+  else if (weatherType == "fog") {
+    if (indexNow == -1 && indexLater != -1) {
+     
+      Serial.println("Oh no! It is going to " + weatherType + " later! Predicted " + later);
+    }
+    
+  }
+  // can't remember last time I saw hail anywhere but just in case
+  else if (weatherType == "clouds") { 
+   if (indexNow == -1 && indexLater != -1) {
+     
+      Serial.println("Oh no! It is going to " + weatherType + " later! Predicted " + later);
+   }
+
+  }
+  // for clear sky, if the current weather does not contain the word clear and the later message does, send notification that it will be sunny later
+  else {
+    if (indexNow == -1 && indexLater != -1) {
+      Serial.println("It is going to be sunny later! Predicted " + later);
+    }
+  }
+}
+
+String SendHTML(float Temperaturestat,float Humiditystat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<meta http-equiv=\"refresh\" content=\"2\" >\n";
+  ptr +="<link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400,600\" rel=\"stylesheet\">\n";
+   ptr +="<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\">\n";
+  ptr +="<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\">\n";
+  ptr +="</script>\n";
+  ptr +="<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js\">\n";
+  ptr +="</script>\n";
+  ptr +="<title>ESP8266 Weather Report</title>\n";
+  ptr +="<style>html { font-family: 'Open Sans', sans-serif; display: block; margin: 0px auto; text-align: center;color: #333333;}\n";
+  ptr +="body{margin-top: 50px;}\n";
+  ptr +="h1 {margin: 50px auto 30px;}\n";
+  ptr +=".side-by-side{display: inline-block;vertical-align: middle;position: relative;}\n";
+  ptr +=".humidity-icon{background-color: #3498db;width: 30px;height: 30px;border-radius: 50%;line-height: 36px;}\n";
+  ptr +=".humidity-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
+  ptr +=".humidity{font-weight: 300;font-size: 60px;color: #3498db;}\n";
+  ptr +=".temperature-icon{background-color: #f39c12;width: 30px;height: 30px;border-radius: 50%;line-height: 40px;}\n";
+  ptr +=".temperature-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
+  ptr +=".temperature{font-weight: 300;font-size: 60px;color: #f39c12;}\n";
+  ptr +=".superscript{font-size: 17px;font-weight: 600;position: absolute;right: -20px;top: 15px;}\n";
+  ptr +=".data{padding: 10px;}\n";
+  ptr +="</style>\n";
+ ptr +="<style>button { position: fixed; bottom: 10%; right: 42%; } </style>\n";
+ptr +="<style>\n";
+ptr +=".navbar { overflow: hidden; background-color: #333;   position: fixed; bottom: 0; right: 42%; width: 10; }\n";
+
+ptr +=".navbar a { float: left; display: block; color: #f2f2f2; text-align: center; padding: 14px 16px; text-decoration: none; font-size: 17px; }\n";
+
+ptr +=".navbar a:hover { background-color: #ddd; color: black; }\n";
+
+ptr +=".navbar a.active { background-color: #4CAF50; color: white; }\n";
+
+ptr +=".navbar .icon { display: none; }\n";
+
+ptr +="@media screen and (max-width: 600px) {\n";
+ ptr +=".navbar a:not(:first-child) {display: none;}\n";
+  ptr+=".navbar a.icon { float: centre; display: block; }\n";
+ptr +="}\n";
+
+ptr +="@media screen and (max-width: 600px) {\n";
+ ptr += ".navbar.responsive .icon { position: absolute; right: 50%; bottom:0; }\n";
+  ptr +=".navbar.responsive a { float: none; display: block; text-align: left; }\n";
+
+ptr +="}\n";
+ptr +="</style>\n";
+ 
+  ptr +="<script>\n";
+  ptr +="setInterval(loadDoc,200);\n";
+  ptr +="function loadDoc() {\n";
+  ptr +="var xhttp = new XMLHttpRequest();\n";
+  ptr +="xhttp.onreadystatechange = function() {\n";
+  ptr +="if (this.readyState == 4 && this.status == 200) {\n";
+  ptr +="document.getElementById(\"webpage\").innerHTML =this.responseText}\n";
+  ptr +="};\n";
+  ptr +="xhttp.open(\"GET\", \"/\", true);\n";
+  ptr +="xhttp.send();\n";
+  ptr +="}\n";
+  ptr +="</script>\n";
+
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+
+  
+   ptr +="<div id=\"webpage\">\n";
+   
+   ptr +="<h1>ESP8266 Weather Report</h1>\n";
+   ptr +="<div class=\"data\">\n";
+   ptr +="<div class=\"side-by-side temperature-icon\">\n";
+   ptr +="<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n";
+   ptr +="width=\"9.915px\" height=\"22px\" viewBox=\"0 0 9.915 22\" enable-background=\"new 0 0 9.915 22\" xml:space=\"preserve\">\n";
+   ptr +="<path fill=\"#FFFFFF\" d=\"M3.498,0.53c0.377-0.331,0.877-0.501,1.374-0.527C5.697-0.04,6.522,0.421,6.924,1.142\n";
+   ptr +="c0.237,0.399,0.315,0.871,0.311,1.33C7.229,5.856,7.245,9.24,7.227,12.625c1.019,0.539,1.855,1.424,2.301,2.491\n";
+   ptr +="c0.491,1.163,0.518,2.514,0.062,3.693c-0.414,1.102-1.24,2.038-2.276,2.594c-1.056,0.583-2.331,0.743-3.501,0.463\n";
+   ptr +="c-1.417-0.323-2.659-1.314-3.3-2.617C0.014,18.26-0.115,17.104,0.1,16.022c0.296-1.443,1.274-2.717,2.58-3.394\n";
+   ptr +="c0.013-3.44,0-6.881,0.007-10.322C2.674,1.634,2.974,0.955,3.498,0.53z\"/>\n";
+   ptr +="</svg>\n";
+   ptr +="</div>\n";
+   ptr +="<div class=\"side-by-side temperature-text\">Temperature</div>\n";
+   ptr +="<div class=\"side-by-side temperature\">";
+   ptr +=(int)Temperaturestat;
+
+
+ 
+   
+   ptr +="<span class=\"superscript\">°C</span></div>\n";
+             ptr +="</div>\n";
+                ptr +="<div class=\"container\">\n";
+             ptr +="<a href=\"https://thingspeak.com/channels/756646/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15\" class=\"btn btn-info\" role=\"button\">temperature</a>\n";
+  
+   ptr +="<div class=\"data\">\n";
+   ptr +="<div class=\"side-by-side humidity-icon\">\n";
+   ptr +="<svg version=\"1.1\" id=\"Layer_2\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n\"; width=\"12px\" height=\"17.955px\" viewBox=\"0 0 13 17.955\" enable-background=\"new 0 0 13 17.955\" xml:space=\"preserve\">\n";
+   ptr +="<path fill=\"#FFFFFF\" d=\"M1.819,6.217C3.139,4.064,6.5,0,6.5,0s3.363,4.064,4.681,6.217c1.793,2.926,2.133,5.05,1.571,7.057\n";
+   ptr +="c-0.438,1.574-2.264,4.681-6.252,4.681c-3.988,0-5.813-3.107-6.252-4.681C-0.313,11.267,0.026,9.143,1.819,6.217\"></path>\n";
+   ptr +="</svg>\n";
+   ptr +="</div>\n";
+   ptr +="<div class=\"side-by-side humidity-text\">Humidity</div>\n";
+   ptr +="<div class=\"side-by-side humidity\">";
+    ptr +=(int)Humiditystat;
+
+    
+  
+   
+   ptr +="<span class=\"superscript\">%</span></div>\n";
+   ptr +="</div>\n";
+    ptr +="<div class=\"container\">\n";
+  ptr +="<a href=\"https://thingspeak.com/channels/756646/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15\" class=\"btn btn-info\" role=\"button\">humidity</a>\n";
+
+
+
+  
+    
+                ptr+="<div class=\"navbar\" id=\"myNavbar\">\n";
+  ptr+="<a  href=\"index.html\" class=\"active\">predict weather</a>\n";
+ 
+
+ptr+="</div>\n";
+
+  ptr +="</div>\n";
+ 
+  
+ ptr+="<div id=\"chartContainer\" style=\"height: 300px; width: 100%;\">\n";
+ 
+ ptr+="</div>\n";
+ptr+="<script src=\"https://canvasjs.com/assets/script/canvasjs.min.js\">\n";
+ptr+="</script>\n";
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
+
+
+
+
+
+
+// !!! call web page http://api.openweathermap.org/data/2.5/forecast?id=1277333&APPID=b429c36fad5233e2e9a25c979f55b08c
+
+
+
+
+
+
+
+
