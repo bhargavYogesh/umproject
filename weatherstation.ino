10cbee48e6d79d2408ab3604d4ead61ce74d73e2
bhargavYogesh <47472492+bhargavYogesh@users.noreply.github.com>
2019-11-15 14:16:05 +0530

    Update weatherstation.ino

--- weatherstation.ino
+++ weatherstation.ino
@@ -1,5 +1,3 @@
-/* Copyright (C) 2019 J Y K BHARGAV */
-
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -465,6 +463,20 @@ String SendHTML(float Temperaturestat, float Humiditystat) {
   return ptr;
 }
 
+/* 
+  Copyright (C) 2019 J Y K BHARGAV
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 
